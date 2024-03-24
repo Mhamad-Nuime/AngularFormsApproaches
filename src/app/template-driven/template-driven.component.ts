746f178c82e0d9da07a77c26ef856524a1b21fa2
Muhamad Nuime <moesnua@gmail.com>
2024-03-24 22:56:53 +0300

    The Finall changes on the forms

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,37 +1,54 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { UserInfo } from '../../interfaces/userInfo.interface';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-template-driven',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   template: `
     <div class="reactive">
-      <form action="">
+      <form (ngSubmit)="onSubmit()" >
         <h2>Template Driven Login Form</h2>
         <p>
-          <label for="email"></label
-          ><input
+          <label for="email"></label>
+          <input
             type="email"
-            id="email"
-            name="user-email"
+            id="Temail"
+            name="userEmail"
+            value=""
             placeholder="Enter Your Email :"
+            [(ngModel)]="this.userInfo.userEmail"
           />
         </p>
         <p>
-          <label for="password"></label
-          ><input
+          <label for="password"></label>
+          <input
             type="password"
-            id="password"
-            name="user-password"
+            id="Tpassword"
+            name="userPassword"
             placeholder="Enter Your Password :"
+            [(ngModel)]="this.userInfo.userPassword"
           />
         </p>
-        <button type="submit">OK</button>
+        <button type="submit" >OK</button>
       </form>
     </div>
+    <br><br>
+      <pre>{{userInfo | json}}</pre>
   `,
-  styleUrl: './template-driven.component.css'
+  styleUrl: './template-driven.component.css',
 })
 export class TemplateDrivenComponent {
-
+  userInfo : UserInfo = {
+    userEmail: '',
+    userPassword: ''
+  }
+  onSubmit() {
+    console.log(`this is the user email : ${this.userInfo.userEmail}, and the password is : ${this.userInfo.userPassword}`);
+    this.userInfo = {
+      userEmail: '',
+      userPassword: ''
+    }
+  }
 }
